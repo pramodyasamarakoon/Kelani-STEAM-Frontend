29d688051adc75d687402075b298330d053312eb
Pramodya Samarakoon <pramodyasamarakoon2516@gmail.com>
2023-11-23 14:04:49 +0530

    11-23 Afternoon

--- src/Pages/OrderTShirtFormPage/index.jsx
+++ src/Pages/OrderTShirtFormPage/index.jsx
@@ -242,10 +242,15 @@ const OrderTShirtFormPage = () => {
   return (
     <div>
       <NavBar visibilityOfOrderButton={false} />
-      {/* T shirt form Container */}
+
+      {/* T shirt form Container PC */}
       <div
-        className="w-[86%] mx-auto"
-        style={{ background: "#D9D9D950", borderRadius: "30px" }}
+        className="w-[86%] mx-auto hidden md:block"
+        style={{
+          background: "#D9D9D950",
+          borderRadius: "30px",
+          // display: { xs: "hidden", md: "block" },
+        }}
       >
         <Grid container spacing={0} sx={{ marginTop: 8 }}>
           {/* Form Title */}
@@ -571,6 +576,437 @@ const OrderTShirtFormPage = () => {
                     {/* Payment Method Radio Group */}
                     <FormControl>
                       <FormLabel sx={{ color: "black" }} id="paymentMethod">
+                        Payment Method
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        name="paymentMethod"
+                        // onChange={handlePaymentMethodChange}
+                        value={formData.paymentMethod}
+                        onChange={handleChangeRadio}
+                      >
+                        <FormControlLabel
+                          value="BankDeposit/OnlinePayment"
+                          control={<Radio />}
+                          label="Bank Deposit/Online Payment"
+                        />
+                        <FormControlLabel
+                          value="CashOnHand"
+                          control={<Radio />}
+                          label="Cash on hand"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
+
+                  <div className="w-full my-8">
+                    {/* File Upload Button */}
+                    <FormLabel
+                      sx={{ color: "black", marginRight: 4 }}
+                      id="file"
+                    >
+                      {formData.paymentMethod === "BankDeposit/OnlinePayment"
+                        ? "Upload an image of your payment proof"
+                        : formData.paymentMethod === "CashOnHand"
+                        ? "Upload the payment proof image you got"
+                        : "Please select the Payment Method"}
+                    </FormLabel>
+                    {/* <Button
+                      component="label"
+                      variant="outlined"
+                      startIcon={<CloudUploadIcon />}
+                      disabled={formData.paymentMethod === ""}
+                    >
+                      Upload file
+                      <VisuallyHiddenInput
+                        type="file"
+                        onChange={handleFileChange}
+                      />
+                    </Button> */}
+                    <label>
+                      <Button
+                        component="span"
+                        variant="outlined"
+                        startIcon={<CloudUploadIcon />}
+                        disabled={
+                          uploadProgress > 0 ||
+                          isFileUploaded ||
+                          !formData.paymentMethod
+                        }
+                      >
+                        {isFileUploaded ? "Done" : "Upload file"}
+                      </Button>
+                      <input
+                        type="file"
+                        style={{ display: "none" }}
+                        onChange={handleFileChange}
+                        disabled={uploadProgress > 0 || isFileUploaded}
+                      />
+                    </label>
+
+                    {/* Progress Bar */}
+                    {uploadProgress > 0 && !isFileUploaded && (
+                      <LinearProgress
+                        variant="determinate"
+                        value={uploadProgress}
+                      />
+                    )}
+                  </div>
+
+                  {/* Submit Button */}
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </Button>
+                </ValidatorForm>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      {/* T shirt form Container Mobile */}
+      <div
+        className="w-[86%] mx-auto block md:hidden"
+        style={{
+          background: "#D9D9D950",
+          borderRadius: "30px",
+          // display: { xs: "hidden", md: "block" },
+        }}
+      >
+        <Grid container spacing={0} sx={{ marginTop: 8 }}>
+          {/* Form Title */}
+          <Grid item xs={12} sx={{ marginY: "20px" }}>
+            <p className="font-OpenSans-SemiBold text-[20px]">
+              Kelani STEAM Member's T Shirt Order Form
+            </p>
+          </Grid>
+          <Grid item xs={12} sx={{}}>
+            {/* White background box */}
+            <div
+              className="w-full h-auto mx-auto"
+              style={{
+                background: "#FFFFFF90",
+                padding: "15px",
+                position: "relative",
+              }}
+            >
+              {/* Shirt Image */}
+              <div className="w-full h-auto ">
+                <img className="" src={TShirtSmall} alt="T Shirt Flyer" />
+              </div>
+              {/* Right text grid */}
+              <div
+                style={{
+                  // position: "absolute",
+                  // left: "69%",
+                  // top: "50%",
+                  // transform: "translate(-50%, -50%)",
+                  marginTop: 15,
+                  width: "full",
+                  textAlign: "left",
+                }}
+              >
+                <Grid container spacing={1}>
+                  {/* T shirt details */}
+                  <Grid item xs={12} sx={{}}>
+                    <p className="font-OpenSans-SemiBold text-[16px]">
+                      T-Shirt Details
+                    </p>
+                    <Grid container spacing={1}>
+                      <Grid item xs={4} sx={{}}>
+                        <p className="font-OpenSans-regular text-[14px]">
+                          Color - Black
+                        </p>
+                      </Grid>
+                      <Grid item xs={4} sx={{}}>
+                        <p className="font-OpenSans-regular text-[14px]">
+                          Material: 220 gsm crocodile
+                        </p>
+                      </Grid>
+                      <Grid item xs={4} sx={{}}>
+                        <p className="font-OpenSans-regular text-[14px]">
+                          Price: 1800.00 LKR
+                        </p>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  {/* Instructions */}
+                  <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <div
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "relative",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "full",
+                          color: "black",
+                          fontSize: 16,
+                          fontFamily: "Open Sans",
+                          fontWeight: "600",
+                          wordWrap: "break-word",
+                        }}
+                      >
+                        Online Payment Details
+                      </div>
+                      <div
+                        style={{
+                          color: "black",
+                          fontSize: 14,
+                          fontFamily: "Open Sans",
+                          fontWeight: "400",
+                          wordWrap: "break-word",
+                        }}
+                      >
+                        ***Note that if you are willing to do the initial
+                        payment using online payment method, you have to do the
+                        payment before submitting the form.
+                        <br />
+                        <br />
+                        Account No: 055200420012203
+                        <br />
+                        Account Holder: SNYA GUNASEKARA
+                        <br />
+                        Bank: Peoples' Bank
+                        <br />
+                        Branch: Kelaniya
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <div
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "relative",
+                      }}
+                    >
+                      <div
+                        style={{
+                          // width: "full",
+                          color: "black",
+                          fontSize: 16,
+                          fontFamily: "Open Sans",
+                          fontWeight: "600",
+                          wordWrap: "break-word",
+                        }}
+                      >
+                        Handover Details
+                      </div>
+                      <div
+                        style={{
+                          width: "100%",
+                          color: "black",
+                          fontSize: 14,
+                          fontFamily: "Open Sans",
+                          fontWeight: "400",
+                          wordWrap: "break-word",
+                        }}
+                      >
+                        ***Note that if you are willing to hand over the initial
+                        payment, you have to do the payment to one of below
+                        mentioned members before submitting the form.
+                        <br />
+                        <br />
+                        Sithumi: 071 495 6554
+                        <br />
+                        Pathum: 077 217 3607
+                      </div>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div>
+
+              {/* Deadline */}
+              <div
+                style={{
+                  // position: "absolute",
+                  // left: "20%",
+                  // top: "90%",
+                  // transform: "translate(-50%, -50%)",
+                  // width: "415px",
+                  marginTop: 10,
+                  textAlign: "center",
+                }}
+              >
+                <p className="font-OpenSans-SemiBold text-[18px]">
+                  Deadline: 30th October 2023
+                </p>
+              </div>
+            </div>
+
+            {/* Form */}
+            <div className="w-full mx-auto my-10">
+              <div
+                style={{
+                  width: "100%",
+                  textAlign: "center",
+                  color: "black",
+                  fontSize: 24,
+                  fontFamily: "Open Sans",
+                  fontWeight: "400",
+                  wordWrap: "break-word",
+                }}
+              >
+                Order Details
+              </div>
+              <div className="w-full my-2" style={{ position: "relative" }}>
+                {/* Shirt Image */}
+                <div className="w-full h-auto ">
+                  <img className="" src={SizeChartSmall} alt="T Shirt Flyer" />
+                </div>
+                <div
+                  style={{
+                    // position: "absolute",
+                    // left: "68%",
+                    // top: "50%",
+                    // transform: "translate(-50%, -50%)",
+                    textAlign: "left",
+                    // width: "570px",
+                    marginTop: 10,
+                  }}
+                >
+                  <ValidatorForm>
+                    {/* Name */}
+                    <TextValidator
+                      name="name"
+                      label="Name"
+                      variant="standard"
+                      fullWidth
+                      required
+                      sx={{
+                        marginY: "5px",
+                      }}
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
+
+                    {/* Department */}
+                    <SelectValidator
+                      fullWidth
+                      label="Department"
+                      variant="standard"
+                      id="department"
+                      name="department"
+                      value={formData.department}
+                      onChange={handleChange}
+                      sx={{ marginY: "5px" }}
+                    >
+                      {departments.map((dept) => (
+                        <MenuItem key={dept} value={dept}>
+                          {dept}
+                        </MenuItem>
+                      ))}
+                    </SelectValidator>
+
+                    {/* Student Number */}
+                    <TextValidator
+                      name="studentNumber"
+                      label="Student Number"
+                      variant="standard"
+                      fullWidth
+                      required
+                      sx={{
+                        marginY: "5px",
+                      }}
+                      value={formData.studentNumber}
+                      onChange={handleChange}
+                    />
+
+                    {/* Contact Number (Whatsapp Preferred) */}
+                    <TextValidator
+                      name="contactNumber"
+                      label={
+                        Boolean(contactNumberError)
+                          ? contactNumberError
+                          : "Contact Number (Whatsapp Preferred)"
+                      }
+                      variant="standard"
+                      fullWidth
+                      required
+                      error={Boolean(contactNumberError)}
+                      sx={{
+                        marginY: "5px",
+                      }}
+                      value={formData.contactNumber}
+                      onChange={handleChange}
+                    />
+
+                    {/* E-mail */}
+                    <TextValidator
+                      name="email"
+                      label={Boolean(emailError) ? emailError : "E-mail"}
+                      variant="standard"
+                      fullWidth
+                      required
+                      error={Boolean(emailError)}
+                      sx={{
+                        marginY: "5px",
+                      }}
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+
+                    {/* Size */}
+                    <SelectValidator
+                      fullWidth
+                      label="Size"
+                      variant="standard"
+                      id="size"
+                      name="size"
+                      value={formData.size}
+                      onChange={handleChange}
+                      sx={{ marginY: "5px" }}
+                    >
+                      {sizeChart.map((size) => (
+                        <MenuItem key={size} value={size}>
+                          {size}
+                        </MenuItem>
+                      ))}
+                    </SelectValidator>
+                  </ValidatorForm>
+                </div>
+              </div>
+              <div className="w-full mt-6">
+                <ValidatorForm>
+                  <div className="">
+                    {/* Payment Amount Radio Group */}
+                    <FormControl id="paymentAmount">
+                      <FormLabel sx={{ color: "black" }}>
+                        Payment Amount
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        name="paymentAmount"
+                        value={formData.paymentAmount}
+                        onChange={handleChangeRadio}
+                      >
+                        <FormControlLabel
+                          value="1800"
+                          control={<Radio />}
+                          label="Full Payment (LKR 1800)"
+                        />
+                        <FormControlLabel
+                          value="1000"
+                          control={<Radio />}
+                          label="Half Payment (LKR 1000)"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+
+                    {/* Payment Method Radio Group */}
+                    <FormControl>
+                      <FormLabel
+                        sx={{ color: "black", marginTop: 5 }}
+                        id="paymentMethod"
+                      >
                         Payment Method
                       </FormLabel>
                       <RadioGroup
