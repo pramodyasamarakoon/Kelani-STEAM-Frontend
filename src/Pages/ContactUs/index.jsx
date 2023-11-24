021a3657560c5502b0dae5a3d8ae772fb1b5a22d
Pramodya Samarakoon <pramodyasamarakoon2516@gmail.com>
2023-11-24 16:26:17 +0530

    11-24 Morning

--- src/Pages/ContactUs/index.jsx
+++ src/Pages/ContactUs/index.jsx
@@ -11,15 +11,15 @@ const ContactUs = () => {
 
   // default icon styles
   const defaultIconStyles = {
-    width: 30,
-    height: 30,
+    width: { xs: 20, md: 30 },
+    height: { xs: 20, md: 30 },
     cursor: "pointer",
   };
 
   // default icon styles
   const selectedIconStyles = {
-    width: 44,
-    height: 44,
+    width: { xs: 34, md: 44 },
+    height: { xs: 34, md: 44 },
     cursor: "pointer",
     boxShadow: "0px 2px 3px 1px rgba(0, 0, 0, 0.5)",
   };
@@ -32,7 +32,7 @@ const ContactUs = () => {
       <NavBar />
       <ScrollToTopButton />
 
-      {/* Hero Section */}
+      {/* Hero Section PC */}
       <Grid
         container
         spacing={2}
@@ -192,7 +192,172 @@ const ContactUs = () => {
           </Box>
         </div>
       </Grid>
-      {/* </Grid> */}
+
+      {/* Hero Section Mobile */}
+      <Grid
+        container
+        spacing={2}
+        className="h-[500px]"
+        sx={{
+          marginTop: 12,
+          marginBottom: 5,
+          width: "100%",
+          marginX: "auto",
+          display: { xs: "flex", md: "none" },
+          // borderRadius: "20px",
+          backgroundColor: "#e0e0e0",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+        }}
+      >
+        {/* Icons Grid */}
+        <div
+          style={{
+            width: "10%",
+            height: "full",
+            position: "absolute",
+            left: "10%",
+            top: "25%",
+            transform: "translate(-50%, -50%)",
+            backgroundColor: "#d0d0d0",
+            borderRadius: "16px",
+            backdropFilter: "blur(10px)",
+            zIndex: 99,
+            // paddingTop: 4,
+          }}
+        >
+          <Stack
+            direction="column"
+            spacing={1}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "10px",
+            }}
+          >
+            <Avatar
+              alt="Facebook Icon"
+              src="https://cdn-icons-png.flaticon.com/128/145/145802.png"
+              sx={
+                selectedIcon === "facebook"
+                  ? selectedIconStyles
+                  : defaultIconStyles
+              }
+              onClick={() => handleContact("facebook")}
+              size="small"
+            />
+            <Avatar
+              alt="Instagram Icon"
+              src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"
+              sx={
+                selectedIcon === "instagram"
+                  ? selectedIconStyles
+                  : defaultIconStyles
+              }
+              onClick={() => handleContact("instagram")}
+            />
+            <Avatar
+              alt="linkedin Icon"
+              src="https://cdn-icons-png.flaticon.com/128/145/145807.png"
+              sx={
+                selectedIcon === "linkedin"
+                  ? selectedIconStyles
+                  : defaultIconStyles
+              }
+              onClick={() => handleContact("linkedin")}
+            />
+            <Avatar
+              alt="G Mail Icon"
+              src="https://cdn-icons-png.flaticon.com/128/888/888853.png"
+              sx={
+                selectedIcon === "gmail"
+                  ? selectedIconStyles
+                  : defaultIconStyles
+              }
+              onClick={() => handleContact("gmail")}
+            />
+            <Avatar
+              alt="Whatsapp Icon"
+              src="https://cdn-icons-png.flaticon.com/128/3670/3670051.png"
+              sx={
+                selectedIcon === "whatsapp"
+                  ? selectedIconStyles
+                  : defaultIconStyles
+              }
+              onClick={() => handleContact("whatsapp")}
+            />
+          </Stack>
+        </div>
+        {/*  Image Container */}
+        <div
+          style={{
+            width: "100%",
+            height: "250px",
+            backgroundColor: "#ffffff",
+            backgroundImage:
+              selectedIcon === "facebook"
+                ? `url(${facebook})`
+                : selectedIcon === "instagram"
+                ? `url(${facebook})`
+                : null,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            // borderRadius: "20px",
+            position: "absolute",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        ></div>
+        {/*  text grid */}
+        <div
+          className="h-[250px]"
+          style={{
+            width: "80%",
+            // height: "460px",
+            position: "absolute",
+            top: "75%",
+            left: "50%",
+            // padding: 18,
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <p className="font-Poppins-SemiBold text-2xl mb-2 mt-4">
+            {selectedIcon === "facebook"
+              ? "Official Facebook Page"
+              : selectedIcon === "instagram"
+              ? "Official Instagram Page"
+              : selectedIcon === "linkedin"
+              ? "Official linkedin Account"
+              : selectedIcon === "gmail"
+              ? "Connect with the E mail"
+              : selectedIcon === "whatsapp"
+              ? "Send a message from Whatsapp"
+              : null}
+          </p>
+          <p className="font-Poppins-regular text-[14px] ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            modi dolores voluptas sapiente autem quasi quia laudantium quis odio
+            harum nostrum quaerat tenetur,Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.
+          </p>
+          <Box sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}>
+            <Button
+              variant="contained"
+              // onClick={handleCloseNavMenu}
+              size="small"
+              sx={{
+                color: "black",
+                display: "end",
+                justifyContent: "end",
+              }}
+            >
+              Visit
+            </Button>
+          </Box>
+        </div>
+      </Grid>
 
       {/* Footer */}
       <Footer />
